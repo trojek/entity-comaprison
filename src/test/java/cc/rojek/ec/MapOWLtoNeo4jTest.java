bb9910c9a9e97fedd3dcd102f72afd971db54e3d
Tomasz Rojek <tomek@rojek.cc>
2013-12-20 22:35:27 +0100

    add junit MapOWLtoNeo4jTest class

---
+++ src/test/java/cc/rojek/ec/MapOWLtoNeo4jTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package cc.rojek.ec;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author tomasz
+ *
+ */
+public class MapOWLtoNeo4jTest {
+
+	/**
+	 * Test method for {@link cc.rojek.ec.MapOWLtoNeo4j#getReadableName(java.lang.String)}.
+	 */
+	@Test
+	public final void testGetReadableName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
