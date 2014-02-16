a83b6791ab8c51d93eb4409aa74686975a3aa7e8
Tomasz Rojek <tomek@rojek.cc>
2014-02-16 08:55:05 +0100

    make logical sequence of program flow

--- src/main/java/cc/rojek/ec/Main.java
+++ src/main/java/cc/rojek/ec/Main.java
@@ -1,13 +1,7 @@
 package cc.rojek.ec;
 
-import java.io.File;
-
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import cc.rojek.ec.algorithm1.JoinOntology;
 import cc.rojek.ec.algorithm2.CompareObject;
@@ -23,23 +17,20 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
-		//OWLOntology ontology = OntologyHelper.loadOntologyFromFile(ONTOLOGY_URL);
-
-		//GraphDatabaseService db = Neo4jHelper.startNeo4jDB(DB_PATH);
-
-		//Neo4jAndOWL exampleon = new Neo4jAndOWL(db, ontology);
-
-		//exampleon.importOntology();
-		
-		//CompareObject compareResult = new CompareObject(db, 13l);
-		//WcompareResult.compareObjectsWith();
+		//String json = "{'object' : 'object1', 'name' : 'Turin', connections: { 'Location' : 'Italy', 'Season' : 'Summer' } } }";
+		ApplicationDomainModel test = new ApplicationDomainModel("olympic");
+		//test.addData(json, "objects");
 		
 		JoinOntology jOnto = new JoinOntology(ONTOLOGY_URL);
-		jOnto.createIndividualNodeConnectedToClass("object10", "Vancouver");
+		test.joinObjectNodesWithOntology("objects", jOnto);
 		
-		//String json = "{'object' : 'object1', 'name' : 'Turin', connections: { 'Location' : 'Italy', 'Season' : 'Summer' } } }";
-		//ApplicationDomainModel test = new ApplicationDomainModel("olympic");
-		//test.addData(json, "objects");
-		//test.printData("objects");
+		// OWLOntology ontology = OntologyHelper.loadOntologyFromFile(ONTOLOGY_URL);
+		// GraphDatabaseService db = Neo4jHelper.startNeo4jDB(DB_PATH);
+
+		// Neo4jAndOWL exampleon = new Neo4jAndOWL(db, ontology);
+		// exampleon.importOntology();
+		
+		// CompareObject compareResult = new CompareObject(db, 13l);
+		// compareResult.compareObjectsWith();
 	}
 }
