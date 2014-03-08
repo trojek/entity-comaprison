621c53c0d26f860b8a323cb3dd481e932bd51e7a
Tomasz Rojek <tomek@rojek.cc>
2014-03-08 18:26:26 +0100

    change tab into spaces + create scaffold

--- src/main/java/cc/rojek/ec/application_domain_model/ApplicationDomainModel.java
+++ src/main/java/cc/rojek/ec/application_domain_model/ApplicationDomainModel.java
@@ -1,8 +1,18 @@
 package cc.rojek.ec.application_domain_model;
 
+import java.util.ArrayList;
 
 public class ApplicationDomainModel {
 
-
-
+	public ArrayList<Long> comparisonCriteria = new ArrayList<Long>(); 
+	
+	/*
+	 * Method buildModel should give us proposals comparison criteria
+	 * During building model it is important to do not add node to comparisonCriteria which is a  child/parent (or deeper) of current comparisonCriteria node.
+	 */
+	public void buildModel(){
+		
+	}
+	
+	public void checkIfObjectsAreAddedCorrect(){}
 }
