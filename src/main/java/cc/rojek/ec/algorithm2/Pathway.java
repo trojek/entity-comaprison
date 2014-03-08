84c919bf07790b81a3569c6e28ad765d32335f8f
Tomasz Rojek <tomek@rojek.cc>
2014-03-08 18:24:24 +0100

    change tab into spaces

--- src/main/java/cc/rojek/ec/algorithm2/Pathway.java
+++ src/main/java/cc/rojek/ec/algorithm2/Pathway.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 
 public class Pathway {
 
-	public long individualId;
-	public long groupId;
-	public ArrayList<Long> path = new ArrayList<Long>();
-	
-	Pathway(long individualId){
-		this.individualId = individualId;
-	}
-	
-	public void setGroupId(long groupId) {
-		this.groupId = groupId;
-	}
-	
-	public void add(long l) {
-		path.add(l);
-	}
+    public long individualId;
+    public long groupId;
+    public ArrayList<Long> path = new ArrayList<Long>();
+
+    Pathway(long individualId) {
+        this.individualId = individualId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    public void add(long l) {
+        path.add(l);
+    }
 }
